6225809ffd507202a1c44d0e1c45c8a9c84609ad
Jiaul21 <jiaulhaque.hr@gmail.com>
2024-07-14 02:29:44 +0600

    target

---
+++ src/main/java/com/jiaul/virtualtutor/entities/teacher/dto/TeacherDto.java
@@ -0,0 +1,30 @@
+package com.jiaul.virtualtutor.entities.teacher.dto;
+
+import com.jiaul.virtualtutor.entities.course.Course;
+import com.jiaul.virtualtutor.user.UserCredential;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TeacherDto {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private byte[] photo;
+    private String phone;
+    private String gender;
+    private String language;
+    private String country;
+    private String city;
+    private String bio;
+    private String degree;
+    private List<String> skills;
+
+    private UserCredential userCredential;
+    private List<Course> sellCourses;
+}
