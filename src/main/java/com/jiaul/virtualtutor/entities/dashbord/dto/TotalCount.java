0730a8d905aae179b6ca5571c935020f5dd558e5
Jiaul21 <jiaulhaque.hr@gmail.com>
2024-08-01 23:38:07 +0600

    dashboard

---
+++ src/main/java/com/jiaul/virtualtutor/entities/dashbord/dto/TotalCount.java
@@ -0,0 +1,13 @@
+package com.jiaul.virtualtutor.entities.dashbord.dto;
+
+import lombok.Data;
+
+@Data
+public class TotalCount {
+
+    private String totalStudents;
+    private String totalTeachers;
+    private String totalModules;
+    private String totalAcademicCourses;
+    private String totalSkillCourses;
+}
